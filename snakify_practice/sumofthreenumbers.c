8cb351e8d4678f67c03ee9ad5632526d9d076f85
UzayPoyraz <54606964+UzayPoyraz@users.noreply.github.com>
2020-02-03 21:18:52 +0900

    Create sumofthreenumbers.c

---
+++ snakify_practice/sumofthreenumbers.c
@@ -0,0 +1,6 @@
+# This program will get sum of three numbers inputed
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+c = int(input("enter third number: "))
+sum = a + b + c
+print "The addition is",sum
