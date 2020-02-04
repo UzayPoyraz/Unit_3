f397426486c2bd2ee2ffb579964f531d8a8dc1eb
UzayPoyraz <54606964+UzayPoyraz@users.noreply.github.com>
2020-02-04 15:24:05 +0900

    Create signfunction.py

---
+++ snakify_practice/signfunction.py
@@ -0,0 +1,8 @@
+#This program will print different values depending if the number is positive negative or zero
+a = int(input("Enter a number: "))
+if a>0:
+  print 1
+elif a == 0:
+  print 0
+else:
+  print -1
