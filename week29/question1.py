a7b89ca1daff3ad2d7afaf70c8030e4219cbe1e4
UzayPoyraz <54606964+UzayPoyraz@users.noreply.github.com>
2020-04-14 03:13:43 +0300

    Update question1.py

--- week29/question1.py
+++ week29/question1.py
@@ -1,1 +1,9 @@
-
+f = open("emails.txt", "a")
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+num = input("Enter a number: ")
+times = 0
+for num in range [0, num + 1]:
+  times =+ 1
+f.write(name,".",surname,num,"@uwcisak.jp")
+f.close()
